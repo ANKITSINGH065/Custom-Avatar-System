ab001c178a19cc3fbf9968fb8ff53574b065437a
Ankit Singh <thakurbharat065@gmail.com>
2024-08-28 20:58:04 +0530

    Initial commit for client folder

---
+++ src/config/config.js
@@ -0,0 +1,10 @@
+const config = {
+  development: {
+    backendUrl: "https://custom-art-system-server.onrender.com/api/v1/dalle",
+  },
+  production: {
+    backendUrl: "https://custom-art-system-server.onrender.com/api/v1/dalle",
+  },
+};
+
+export default config;
